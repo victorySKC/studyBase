7bb08aa567d4b117a25f73046892f1c0eb00a4c7
VincentSi <2289799319@qq.com>
2020-08-25 15:21:58 +0800

    冒泡排序与并归排序

--- arithmetic/src/gaoxiaosuanfa/sort/SortMethod.java
+++ arithmetic/src/gaoxiaosuanfa/sort/SortMethod.java
@@ -7,10 +7,11 @@ package gaoxiaosuanfa.sort;
  **/
 public class SortMethod {
     public static void main(String[] args) {
-        int[] list = {12,56,89,16,46,48,415,766,1267,123};
+        int[] list = {12,56,89,16,46,48,415,766,1267,123 ,-23};
 
         //System.out.println(list[2]);
-         SortMethod.insertionSort(list);
+        // SortMethod.insertionSort(list);
+        bubbleSort(list);
 
         for (int i = 0; i <list.length ; i++) {
             System.out.print(" " + list[i]);
@@ -34,5 +35,29 @@ public class SortMethod {
             }
             list[k + 1] = element;
         }
+    }
+
+    public static void bubbleSort(int[] list){
+        /**
+         * @author : Vincent
+         * @description: 冒泡排序、就是下沉排序。需要遍历，所以有for嵌套。
+         * @time: 2020/8/25 14:17
+         * @params [list]
+         * @return void
+         */
+
+        boolean nextNeed =true;
+        for (int i = 1; i < list.length && nextNeed; i++) {
+            nextNeed = false;
+            for (int j = 0; j <list.length - i; j++) {
+                if (list[j] > list[j + 1]){
+                    int temp = list [j];
+                    list [j] = list[j + 1];
+                    list[j + 1] = temp;
+                    nextNeed = true;
+                }
+            }
+        }
+
     }
 }
--- arithmetic/src/gaoxiaosuanfa/sort/Sorting.java
+++ arithmetic/src/gaoxiaosuanfa/sort/Sorting.java
@@ -7,10 +7,11 @@ package gaoxiaosuanfa.sort;
  **/
 public class Sorting {
     public static void main(String[] args) {
-        int[] list = {12,56,89,16,46,48,415,766,1267,123};
+        int[] list = {12,56,89,16,46,48,415,766,1267,123,-465};
 
         //System.out.println(list[2]);
        /* SortMethod.insertionSort(list);*/
+        MergeSort.mergeSort(list);
 
         for (int i = 0; i <list.length ; i++) {
             System.out.print(" " + list[i]);
