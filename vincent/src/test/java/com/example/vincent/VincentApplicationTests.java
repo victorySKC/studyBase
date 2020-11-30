bd59336f4997139f984e156c87f775477ba09382
skc <2289799316@qq.com>
2020-11-30 23:31:44 +0800

    添加独立模块测试成功。之后这个将作为学习的主要项目

---
+++ vincent/src/test/java/com/example/vincent/VincentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vincent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VincentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
