4b0f30552c710373d2b09dc3613ac16d62a2a04f
skc <2289799316@qq.com>
2020-12-06 23:36:22 +0800

    添加mybatis

---
+++ mybatis/src/main/java/com/mybatis/App.java
@@ -0,0 +1,13 @@
+package com.mybatis;
+
+/**
+ * @program: studyBase
+ * @description:
+ * @author: Vincent
+ * @create: 2020/12/6 23:35
+ **/
+public class App {
+    public static void main(String[] args) {
+        System.out.println("ooo");
+    }
+}
