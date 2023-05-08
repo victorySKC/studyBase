ce888cfc7dabf69d8f4b976e36a27713d25f023b
司坤城 <2289799316@qq.com>
2023-05-08 20:43:01 +0800

    Input输入类

---
+++ coreTechnology/src/main/java/v1ch03/InputTest.java
@@ -0,0 +1,25 @@
+package v1ch03;
+
+import java.util.Scanner;
+
+/**
+ * @author SiVincent
+ * @description 测试输入方法  Scanner
+ * @date 2023/5/8
+ */
+public class InputTest {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        // get first input
+        System.out.print("What is your name? ");
+        String name = in.nextLine();
+
+        // get second input
+        System.out.print("How old are you? ");
+        int age = in.nextInt();
+
+        // display output on console
+        System.out.println("Hello, " + name + ". Next year, you'll be " + (age + 1));
+    }
+}
