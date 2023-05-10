51ee0d25a4d8cc0aec78f9d7cd0d1c2c8086fa54
司坤城 <2289799316@qq.com>
2023-05-10 21:12:45 +0800

    类学习over

--- coreTechnology/src/main/java/v1ch04/ConstructorTest.java
+++ coreTechnology/src/main/java/v1ch04/ConstructorTest.java
@@ -37,18 +37,19 @@ class Employee1
     // static initialization block
     static
     {
+        //静态初始化块
         Random generator = new Random();
         // set nextId to a random number between 0 and 9999
         nextId = generator.nextInt(10000);
     }
 
-    // object initialization block
+    // object initialization block 实例域初始化
     {
         id = nextId;
         nextId++;
     }
 
-    // three overloaded constructors
+    // three overloaded constructors 重载构造器
     public Employee1(String n, double s)
     {
         name = n;
@@ -58,12 +59,14 @@ class Employee1
     public Employee1(double s)
     {
         // calls the Employee(String, double) constructor
+        //重载构造器
         this("Employee1 #" + nextId, s);
     }
 
     // the default constructor
     public Employee1()
     {
+        //无参数构造器
         // name initialized to ""--see above
         // salary not explicitly set--initialized to 0
         // id initialized in initialization block
